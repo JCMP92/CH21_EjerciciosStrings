fa8d97f494d9f9c3d4ce415a2b9ab57aaa12a9b5
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2022-12-12 13:46:58 -0600

    Creación de funciones para encontrar el caracter que más se repite

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function mostRepeated(str) {
+  let freqCounter = {};
+  let lowStr = str.toLowerCase();
+  let maxCount = 0;
+  let maxChar = null;
+  let repetition = null;
+
+  for (let char of lowStr) {
+    freqCounter[char] = freqCounter[char] + 1 || 1;
+  }
+
+  for (let key in freqCounter) {
+    if (freqCounter[key] > maxCount) {
+      maxCount = freqCounter[key];
+      maxChar = key;
+      repetition = freqCounter[key];
+    }
+  }
+  return `${maxChar} es el caracter más repetido, con ${repetition} apariciones en ${str}`;
+}
+
+
+
+function repeatedV2(str) {
+  let lowStr = str.toLowerCase();
+  let contFinal = 0;
+  let letra = '';
+
+  for (let i = 0; i < lowStr.length; i++) {
+    let contadorTemp = 1;
+    for (let j = i + 1; j < lowStr.length; j++) {
+      if (lowStr.charAt(i) == lowStr.charAt(j)) contadorTemp++;
+    }
+    if (contadorTemp > contFinal) {
+      contFinal = contadorTemp;
+      letra = lowStr.charAt(i);
+    }
+  }
+
+  return `${letra} se repite ${contFinal} veces`;
+}
