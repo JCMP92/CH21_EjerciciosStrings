618db801e6549a5b2072eaf14ceba7729f36b586
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2022-12-13 10:54:40 -0600

    Validación a RFC

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ btnSend.addEventListener('click', function (e) {
   e.preventDefault();
   let emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let rfcRegex = '';
   let inputMail = document.getElementById('exampleInputEmail1');
   let inputPass = document.getElementById('exampleInputPassword1');
   let alert = document.getElementById('alert');
+  let rfc = document.getElementById('exampleRFC');
   alert.style.display = 'none';
   if (inputPass.value.trim().replaceAll('  ', '').length < 20) {
     alert.innerHTML = 'DEBEN SER MAS DE 20 CARACTERES';
@@ -17,6 +19,10 @@ btnSend.addEventListener('click', function (e) {
   }
   if (inputMail.value.match(emailRegex) == null) {
     alert.innerHTML += '<br/>CORREO INVÁLIDO';
+    alert.style.display = 'block';
+  }
+  if (rfc.value.match(rfcRegex) == null) {
+    alert.innerHTML += '<br/>RFC INVÁLIDO';
     alert.style.display = 'block';
   }
 });
